9b7777cd319834b2371a5e57b890dfe85052d978
tokosource <33731905+tokosource@users.noreply.github.com>
2018-07-12 14:30:31 -0700

    Update Footer.vue

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Azure Global Blackbelt Team
+        Azure Tommy Team
       </div>
       <div class="col-lg-6">
       </div>
